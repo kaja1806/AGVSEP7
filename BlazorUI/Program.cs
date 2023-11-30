bbe227cd40f448997fd5b368dc8b95991ab1f5c4
frence03 <petersen.florence03@gmail.com>
2023-11-30 17:09:06 +0100

    home and other Blazor changes push

---
+++ BlazorUI/Program.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using BlazorUI;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => 
+    new HttpClient
+    {
+        /*BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)*/
+        BaseAddress = new Uri("http://localhost:5004")
+    });
+await builder.Build().RunAsync();
